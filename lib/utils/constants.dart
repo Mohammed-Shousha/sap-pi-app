b55f10cdf89c7a3fb2f84067979dc6cf45a89aed
Mohammed Shousha <mshousha29@gmail.com>
2023-06-13 14:38:44 +0300

    updating constants

--- lib/utils/constants.dart
+++ lib/utils/constants.dart
@@ -1,9 +1,9 @@
 class Constants {
-  static const String baseUrl = "http://192.168.1.18:8000";
+  static const String baseUrl = "http://192.168.2.8:8000";
   static const String paymentUrl =
       "https://sap-backend-production.up.railway.app/payment-sheet";
 
-  static const medicinesUrl = "$baseUrl/medicines";
+  static const medicinesUrl = "$baseUrl/otc-medicines";
   static const orderMedicinesUrl = "$baseUrl/order-medicines";
 
   static String verifyPrescriptionUrl(String prescriptionId) =>
@@ -15,5 +15,5 @@ class Constants {
   static const String logo = 'assets/images/logo.svg';
   static const String prescription = 'assets/images/prescription.png';
   static const String medicine = 'assets/images/medicine.png';
-  static const double imageHeight = 100.0;
+  static const double imageHeight = 150.0;
 }
